eddc8247040739a85e8bf37ef205424e2671265e
Bij4n <b@mawji.vc>
2014-04-14 19:02:00 -0700

    add UIColor+OPTheme header

---
+++ onlyPGP/Categories/UIColor+OPTheme.h
@@ -0,0 +1,21 @@
+//
+//  UIColor+OPTheme.h
+//  onlyPGP
+//
+//  Created 2014. iOS 7 flat design theme colors.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (OPTheme)
+
++ (UIColor *)op_tintColor;
++ (UIColor *)op_greenColor;
++ (UIColor *)op_redColor;
++ (UIColor *)op_orangeColor;
++ (UIColor *)op_grayColor;
++ (UIColor *)op_darkTextColor;
++ (UIColor *)op_lightBackgroundColor;
++ (UIColor *)op_separatorColor;
+
+@end
