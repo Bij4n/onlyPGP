f765b9b5ddff8990d115c69ec3dead23c24c2fe1
Bij4n <b@mawji.vc>
2014-05-06 19:44:09 -0700

    add OPFingerprintView header

---
+++ onlyPGP/Views/OPFingerprintView.h
@@ -0,0 +1,14 @@
+//
+//  OPFingerprintView.h
+//  onlyPGP
+//
+//  Created 2014. ARC enabled.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPFingerprintView : UIView
+
+@property (nonatomic, copy) NSString *fingerprint;
+
+@end
