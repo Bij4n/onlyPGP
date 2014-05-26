68328112a4688ce91073ceb57e72db08a09e56d8
Bij4n <b@mawji.vc>
2014-05-25 17:44:25 -0700

    add QR code chunker header

---
+++ onlyPGP/Services/OPQRCodeChunker.h
@@ -0,0 +1,25 @@
+//
+//  OPQRCodeChunker.h
+//  onlyPGP
+//
+//  Created 2014. Splits armored PGP keys into QR code chunks and reassembles.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPQRCodeChunker : NSObject
+
+// Chunking for display
++ (NSArray *)chunksForArmoredKey:(NSString *)armoredKey maxChunkSize:(NSUInteger)maxSize;
++ (NSUInteger)chunkCountForArmoredKey:(NSString *)armoredKey maxChunkSize:(NSUInteger)maxSize;
+
+// Reassembly from scanned chunks
+- (instancetype)initWithExpectedChunkCount:(NSUInteger)count;
+- (BOOL)addChunkString:(NSString *)chunkString;
+- (BOOL)isComplete;
+- (float)progress;
+- (NSString *)assembledArmoredKey;
+- (NSArray *)missingChunkIndices;
+- (void)reset;
+
+@end
