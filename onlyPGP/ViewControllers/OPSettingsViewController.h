7e3cfada0fd93410a02df55051cde84479cfa937
Bij4n <b@mawji.vc>
2014-06-11 22:22:23 -0700

    stub settings view controller

---
+++ onlyPGP/ViewControllers/OPSettingsViewController.h
@@ -0,0 +1,12 @@
+//
+//  OPSettingsViewController.h
+//  onlyPGP
+//
+//  Created 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPSettingsViewController : UITableViewController <UIActionSheetDelegate, UIAlertViewDelegate>
+
+@end
